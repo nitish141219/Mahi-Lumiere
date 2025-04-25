ca6eea94767ea700a238b0f9effe5899e0035f8f
nitish141219 <koyyalamudi.nitish@gmail.com>
2025-04-25 20:47:05 +0530

    Delete success.js

--- success.js
+++
@@ -1,8 +0,0 @@
-export default function Success() {
-  return (
-    <div className="p-10 text-center">
-      <h1 className="text-3xl font-bold text-green-600">Thank you!</h1>
-      <p className="mt-2 text-gray-600">Your purchase was successful. Enjoy your luxurious Mahi Lumière soap!</p>
-    </div>
-  );
-}
